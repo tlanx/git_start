af117a051f9b36077186d9e0858f546a1a23fff0
tlanx <tlanx@tlanxtekimacbook-pro.local>
2019-11-05 15:27:10 +0800

    Initial Commit

---
+++ git_start/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  git_start
+//
+//  Created by tlanx on 2019/11/5.
+//  Copyright © 2019 tlanx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
